dc9c49182e5ddc78209523832229349c73c6891f
Phan Nguyen Anh <phannguyenanhvn@gmail.com>
2019-03-23 15:37:03 -0400

    High order functions

--- baby.hs
+++ baby.hs
@@ -147,3 +147,60 @@ quicksort (x:xs) =
         greatPart = quicksort [a | a <- xs, a > x]
     in
         smallPart ++ [x] ++ greatPart
+
+-- Curried funtions
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- High order functions
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = [] 
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x 
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+    | f x == True = x : again
+    | otherwise = again
+    where again = filter' f xs
+
+largestDivisible :: (Integral a) => [a]
+largestDivisible = filter' p [100000,99999..0]
+    where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2) 
+    | odd n = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length' (filter' isLong (map chain [1..100]))
+    where isLong x = length' x > 15
+
+
+sum'' :: (Num i) => [i] -> i
+sum'' = foldl (+) 0 
+
+elem'' :: (Eq i) => i -> [i] -> Bool
+elem'' x xs = foldl (\acc y -> if y == x then True else acc) False xs
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl (\acc x -> x : acc) []
+
+maximum'' :: Ord a => [a] -> a
+maximum'' = foldr1 (\x acc -> if x > acc then x else acc)
